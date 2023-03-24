6de08af55d6e0292dc2a56e96f3a047353dd9857
Sprok Dániel <sprokdaniel@gmail.com>
2023-03-24 19:24:26 +0100

    Utkozes checking, korrigalo modositasok

--- player.py
+++ player.py
@@ -16,30 +16,29 @@ class Player:
     def movement(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
-        dx, dy = 0, 0
+        inc_x, inc_y = 0, 0
         speed = s.PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
 
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
-            dx += speed_cos
-            dy += speed_sin
+            inc_x += speed_cos
+            inc_y += speed_sin
 
         if keys[pg.K_s]:
-            dx += -speed_cos
-            dy += -speed_sin
+            inc_x += -speed_cos
+            inc_y += -speed_sin
 
         if keys[pg.K_a]:
-            dx += speed_sin
-            dy += -speed_cos
+            inc_x += speed_sin
+            inc_y += -speed_cos
 
         if keys[pg.K_d]:
-            dx += -speed_sin
-            dy += speed_cos
+            inc_x += -speed_sin
+            inc_y += speed_cos
 
-        self.x += dx
-        self.y += dy
+        self.check_wall_collision(inc_x, inc_y)
 
         if keys[pg.K_LEFT]:
             self.angle -= s.PLAYER_ROT_SPEED * self.game.delta_time
@@ -49,11 +48,20 @@ class Player:
 
         self.angle %= math.tau
 
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, inc_x, inc_y):
+        if self.check_wall(int(self.x + inc_x), int(self.y)):
+            self.x += inc_x
+        if self.check_wall(int(self.x), int(self.y + inc_y)):
+            self.y += inc_y
+
     def draw(self):
-        pg.draw.line(self.game.screen, 'blue', (self.x * 40, self.y * 40),
-                     (self.x * 40 + s.WIDTH * math.cos(self.angle),
-                     self.y * 40 + s.WIDTH * math.sin(self.angle)), 2)
-        pg.draw.circle(self.game.screen, 'yellow', (self.x * 40, self.y * 40), 15)
+        pg.draw.line(self.game.screen, 'blue', (self.x * 80, self.y * 80),
+                     (self.x * 80 + s.WIDTH * math.cos(self.angle),
+                     self.y * 80 + s.WIDTH * math.sin(self.angle)), 2)
+        pg.draw.circle(self.game.screen, 'yellow', (self.x * 80, self.y * 80), 15)
 
     def update(self):
         self.movement()
