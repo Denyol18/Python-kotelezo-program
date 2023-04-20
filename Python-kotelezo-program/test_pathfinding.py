43ff9b26b84159a873476467298dc318f91a1bad
Sprok Dániel <sprokdaniel@gmail.com>
2023-04-20 20:35:12 +0200

    tests

---
+++ Python-kotelezo-program/test_pathfinding.py
@@ -0,0 +1,27 @@
+"""pathfinding.py tesztelő szkriptje"""
+
+import unittest
+import main
+import pathfinding as pa
+
+
+class TestPathFinding(unittest.TestCase):
+    """Tesztosztály a PathFinding osztály tesztelésére"""
+
+    def setUp(self):
+        self.game = main.Game()
+        self.pathfinding = pa.PathFinding(self.game)
+
+    def test_init(self):
+        """Konstruktor tesztelő függvény"""
+
+        self.assertTrue(self.pathfinding.game)  # Legyen értékük
+        self.assertTrue(self.pathfinding.game.map.mini_map)
+        self.assertTrue(self.pathfinding.graph)
+        self.assertFalse(self.pathfinding.get_graph())  # Ne térjen vissza semmivel
+        self.assertFalse(self.pathfinding.visited)  # 0 == False
+
+    def test_get_path(self):
+        """Utat lekérő, annak végével visszatérő függvény tesztelése"""
+
+        self.assertTrue(self.pathfinding.get_path((1, 1), (3, 3)))  # Térjen vissza vmi értékkel
